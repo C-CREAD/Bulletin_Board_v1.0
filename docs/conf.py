47635da55d15efc5489fcd4b0f42a928d3f86899
C-CREAD <shingai.dzinotyiweyi@gmail.com>
2024-09-06 20:23:17 +0200

    Generated necessary HTML documentation using sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ import sys
 import django
 
 sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Your_project_name.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'capatone_project.settings'
 django.setup()
 
 # -- Project information -----------------------------------------------------
@@ -22,7 +22,9 @@ release = '1.00'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
